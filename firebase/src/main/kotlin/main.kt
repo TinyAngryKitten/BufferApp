a8d17ca82974e7f0324c722616fe62f443b34e4e
Sander Høyvik <2852267+TinyAngryKitten@users.noreply.github.com>
2021-07-15 19:02:32 +0200

    Update main.kt

--- firebase/src/main/kotlin/main.kt
+++ firebase/src/main/kotlin/main.kt
@@ -1,6 +1,9 @@
-fun main() {
-    println(greeting("FirebaseFunctionsKt"))
-}
+external fun require(module:String) : dynamic
+external var exports: dynamic
 
-fun greeting(name: String) =
-    "Hello, $name"
+fun main(args: Array<String>) {
+    val fireFunctions = require("firebase-functions")
+    exports.myTestFun = fireFunctions.https.onRequest { request , response ->
+        response.send("Hi from kotlin!")
+    }
+}
